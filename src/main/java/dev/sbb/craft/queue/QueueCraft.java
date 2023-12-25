f2b0496d99092fcc1ba75060caafdc485a307f4a
Saliou Bailo BAH <saliou.bah@etudiant.univ-lr.fr>
2023-12-25 14:57:00 +0100

    Development and testing of QueueCraft class

---
+++ src/main/java/dev/sbb/craft/queue/QueueCraft.java
@@ -0,0 +1,189 @@
+package dev.sbb.craft.queue;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class QueueCraft<T> implements Cloneable{
+    private List<T> elements;
+
+    public QueueCraft() {
+        this.elements = new ArrayList<>();
+    }
+
+    public QueueCraft(List<T> elements) {
+        this.elements = new ArrayList<>(elements);
+    }
+
+    /**
+     * Inserts an element at the end of the queue.
+     * @param element the element to be inserted
+     */
+    public void insert(T element) {
+        elements.add(element);
+    }
+
+    /**
+     * inserts an element at the specified index
+     * @param element the element to be inserted
+     * @param index the index at which the element is to be inserted
+     */
+    public void insert(T element, int index) {
+        elements.add(index, element);
+    }
+
+    /**
+     * Removes the first element of the queue
+     * @return the removed element
+     */
+    public T remove() {
+        return elements.remove(0);
+    }
+
+    /**
+     * Removes the element at the specified index
+     * @param index the index of the element to be removed
+     * @return the removed element
+     */
+    public T remove(int index) {
+        return elements.remove(index);
+    }
+
+    /**
+     * Returns the first element of the queue
+     * @return the first element
+     */
+    public T get() {
+        return elements.get(0);
+    }
+
+    /**
+     * Returns the queue as a list
+     * @return the queue as a list
+     */
+    public List<T> getElements() {
+        return elements;
+    }
+
+    /**
+     * Returns the element at the specified index
+     * @param index the index of the element to be returned
+     * @return the element at the specified index
+     */
+    public T get(int index) {
+        return elements.get(index);
+    }
+
+    /**
+     * Returns the size of the queue
+     * @return the size of the queue
+     */
+    public int size() {
+        return elements.size();
+    }
+
+    /**
+     * Returns the index of the specified element
+     * @param element the element whose index is to be returned
+     * @return the index of the specified element
+     */
+    public int indexOf(T element) {
+        return elements.indexOf(element);
+    }
+
+    /**
+     * Returns true if the queue is empty
+     * @return true if the queue is empty
+     */
+    public boolean isEmpty()
+    {
+        return this.elements.isEmpty();
+    }
+
+    /**
+     * Returns true if the queue contains the specified element
+     * @param element the element whose presence in the queue is to be tested
+     * @return true if the queue contains the specified element
+     */
+    public boolean contains(T element) {
+        return elements.contains(element);
+    }
+
+    /**
+     * Returns true if the queue contains all the elements of the specified queue
+     * @param queue the queue whose elements are to be checked for containment
+     * @return true if the queue contains all the elements of the specified queue
+     */
+    public boolean containsAll(QueueCraft<T> queue) {
+        return queue.elements.stream().allMatch(element -> this.contains(element));
+    }
+
+    /**
+     * Deletes all the elements of the queue
+     * @return  true if the queue is cleared
+     */
+    public boolean clear() {
+        if(!this.isEmpty())
+        {
+            this.elements.clear();
+            return true;
+        }
+        return false;
+    }
+
+    /**
+     * Returns the queue as a string
+     * @return the queue as a string
+     */
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("{");
+        for (T element : elements) {
+            sb.append(element.toString());
+            sb.append(" - ");
+        }
+        sb.delete(sb.length() - 3, sb.length());
+        sb.append("}");
+        return sb.toString();
+    }
+
+    /**
+     * Returns true if the queue is equal to the specified object
+     * @param obj the object to be compared for equality with the queue
+     * @return true if the queue is equal to the specified object
+     */
+    @Override
+    public boolean equals(Object obj) {
+        if (obj instanceof QueueCraft) {
+            QueueCraft<T> other = (QueueCraft<T>) obj;
+            if (this.size() == other.size()) {
+                return other.elements.stream().allMatch(element -> this.contains( (T) element));
+            }
+        }
+        return false;
+    }
+
+    /**
+     * Returns true if the queue is equal to the specified queue
+     * @param other the queue to be compared for equality with the queue
+     * @return true if the queue is equal to the specified queue
+     */
+    private boolean checkIfElementIsEqual(T other) {
+        return elements.stream().allMatch(element -> element.equals(other));
+    }
+
+
+    @Override
+    public QueueCraft<T> clone() {
+        try {
+            QueueCraft<T> clone = (QueueCraft<T>) super.clone();
+            clone.elements = new ArrayList<>();
+            for (T element : this.elements) {
+                clone.insert(element);
+            }
+            return clone;
+        } catch (CloneNotSupportedException e) {
+            throw new AssertionError();
+        }
+    }
+}
---
+++ src/test/java/dev/sbb/craft/queue/QueueCraftTest.java
@@ -0,0 +1,160 @@
+package dev.sbb.craft.queue;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class QueueCraftTest {
+
+    @org.junit.jupiter.api.Test
+    void testSize() {
+        QueueCraft<Integer> queue = new QueueCraft<>();
+        queue.insert(1);
+
+        assertEquals(1, queue.size());
+        assertEquals(1, queue.get());
+    }
+
+    @org.junit.jupiter.api.Test
+    void testInsert() {
+        QueueCraft<String> queue = new QueueCraft<>();
+        queue.insert("A");
+        queue.insert("B", 1);
+
+        assertEquals("A", queue.get(0));
+        assertEquals("B", queue.get(1));
+    }
+
+    @org.junit.jupiter.api.Test
+    void insertAtIndexNonEmptyQueue() {
+        QueueCraft<String> queue = new QueueCraft<>(Arrays.asList("apple", "banana", "orange"));
+        queue.insert("grape", 1);
+
+        assertEquals("grape", queue.get(1));
+        assertEquals("banana", queue.get(2));
+        assertEquals(4, queue.size());
+    }
+
+    @org.junit.jupiter.api.Test
+    void insertAtIndexEmptyQueue() {
+        QueueCraft<Integer> queue = new QueueCraft<>();
+        queue.insert(42, 0);
+
+        assertEquals(42, queue.get(0));
+        assertEquals(1, queue.size());
+    }
+
+    @org.junit.jupiter.api.Test
+    void insertAtIndexOutOfBounds() {
+        QueueCraft<Character> queue = new QueueCraft<>(Arrays.asList('a', 'b', 'c'));
+
+        // Attempt to insert at an index beyond the current size
+        assertThrows(IndexOutOfBoundsException.class, () -> queue.insert('d', 5));
+    }
+    @org.junit.jupiter.api.Test
+    void remove() {
+        QueueCraft<Double> queue = new QueueCraft<>();
+        queue.insert(1.1);
+        queue.insert(2.2);
+
+        assertEquals(1.1, queue.remove());
+        assertEquals(1, queue.size());
+    }
+
+    @org.junit.jupiter.api.Test
+    void testRemove() {
+        QueueCraft<Character> queue = new QueueCraft<>();
+        queue.insert('a');
+        queue.insert('b');
+
+        assertEquals('a', queue.remove(0));
+        assertEquals(1, queue.size());
+    }
+
+    @org.junit.jupiter.api.Test
+    void get() {
+        QueueCraft<String> queue = new QueueCraft<>();
+        queue.insert("apple");
+        queue.insert("banana");
+
+        assertEquals("apple", queue.get());
+    }
+
+    @org.junit.jupiter.api.Test
+    void getElements() {
+        QueueCraft<Integer> queue = new QueueCraft<>(Arrays.asList(1, 2, 3));
+        List<Integer> elements = queue.getElements();
+
+        assertEquals(3, elements.size());
+        assertTrue(elements.contains(1));
+        assertTrue(elements.contains(2));
+        assertTrue(elements.contains(3));
+    }
+
+    @org.junit.jupiter.api.Test
+    void testGet() {
+    }
+
+
+    @org.junit.jupiter.api.Test
+    void indexOf() {
+        QueueCraft<Character> queue = new QueueCraft<>(Arrays.asList('a', 'b', 'c'));
+        assertEquals(1, queue.indexOf('b'));
+        assertEquals(-1, queue.indexOf('z')); // Assuming 'z' is not in the queue
+    }
+
+
+    @org.junit.jupiter.api.Test
+    void isEmpty() {
+        QueueCraft<String> emptyQueue = new QueueCraft<>();
+        QueueCraft<String> nonEmptyQueue = new QueueCraft<>(Arrays.asList("apple", "orange"));
+
+        assertTrue(emptyQueue.isEmpty());
+        assertFalse(nonEmptyQueue.isEmpty());
+    }
+
+    @org.junit.jupiter.api.Test
+    void contains() {
+        QueueCraft<String> queue = new QueueCraft<>(Arrays.asList("apple", "banana", "orange"));
+        assertTrue(queue.contains("banana"));
+        assertFalse(queue.contains("grape")); // Assuming 'grape' is not in the queue
+    }
+
+    @org.junit.jupiter.api.Test
+    void testToString() {
+        QueueCraft<Integer> queue = new QueueCraft<>(Arrays.asList(1, 2, 3));
+        assertEquals("{1 - 2 - 3}", queue.toString());
+    }
+
+    @org.junit.jupiter.api.Test
+    void clearNonEmptyQueue() {
+        QueueCraft<String> queue = new QueueCraft<>(Arrays.asList("apple", "banana", "orange"));
+        assertTrue(queue.clear());
+        assertTrue(queue.isEmpty());
+    }
+
+    @org.junit.jupiter.api.Test
+    void clearEmptyQueue() {
+        QueueCraft<Integer> queue = new QueueCraft<>();
+        assertFalse(queue.clear());
+        assertTrue(queue.isEmpty());
+    }
+
+    @org.junit.jupiter.api.Test
+    void testEquals() {
+        QueueCraft<Integer> queue1 = new QueueCraft<>(Arrays.asList(1, 2, 3));
+        QueueCraft<Integer> queue2 = new QueueCraft<>(Arrays.asList(1, 2, 3));
+
+        assertEquals(queue1, queue2);
+    }
+
+    @org.junit.jupiter.api.Test
+    void testClone() {
+        QueueCraft<Double> originalQueue = new QueueCraft<>(Arrays.asList(1.1, 2.2, 3.3));
+        QueueCraft<Double> clonedQueue = originalQueue.clone();
+
+        assertEquals(originalQueue, clonedQueue);
+        assertNotSame(originalQueue, clonedQueue);
+    }
+}
